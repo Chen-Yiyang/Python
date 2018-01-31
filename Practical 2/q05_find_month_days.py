dff9b0f608be76e7b2f36ed0479048e1ccaf1471
Chen-Yiyang <iycspfa@gmail.com>
2018-01-31 17:58:10 +0800

    Update q05_find_month_days.py
    Correct a small error in the codes

--- Practical 2/q05_find_month_days.py
+++ Practical 2/q05_find_month_days.py
@@ -1,8 +1,8 @@
 # Finding the number of days in a month
 # by Yiyang 29/01/18
 
-def _leap(x): # used to test if its leap year and if yes, adjust the days by +1
-    if (x % 4 == 0 and x % 100 != 0) or (x % 400 == 0):
+def _leapFeb(w, x): # used to test if its Feb in a leap year and if yes, adjust the days by +1
+    if (w == 2) && ((x % 4 == 0 and x % 100 != 0) or (x % 400 == 0)):
         return 1
     else:
         return 0
@@ -13,4 +13,4 @@ monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 monthValue = int(input("Month: "))
 yearValue = int(input("Year: "))
 
-print("{0} {1} has {2} days.".format(monthName[monthValue-1], yearValue, monthLength[monthValue-1] + _leap(yearValue)))
+print("{0} {1} has {2} days.".format(monthName[monthValue-1], yearValue, monthLength[monthValue-1] + _leap(monthValue, yearValue)))
