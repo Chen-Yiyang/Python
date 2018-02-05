61963b97606b77935e4101238d51bf78e6e4c0ee
Gi Soong Chee <gi.soongchee@dhs.sg>
2018-02-05 13:05:50 +0800

    added suggestion to question in comments

--- Practical 2/q07_miles_to_kilometres.py
+++ Practical 2/q07_miles_to_kilometres.py
@@ -7,3 +7,4 @@ for i in range(10):
     print("{0}\t{1:.3f}\t\t{2}\t\t{3:.3f}".format(i+1, (i+1) * 1.609, (i * 5 + 20), (i * 5 + 20)/1.609))
 
 # are there any other ways to print such columns other than \t ?
+# you can use column width in the format specifier eg {0:10} will reserve 10 columns for the first value
